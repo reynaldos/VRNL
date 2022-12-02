b660b2c9f05ed4dbadfe23c787ea8372977c7a0d
reynaldos <reyjr98@gmail.com>
2022-12-02 11:04:42 -0500

    comments CRUD operations

---
+++ server/controllers/comment.js
@@ -0,0 +1,44 @@
+import { createError } from "../error.js";
+import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
+
+
+// ADD COMMENT
+export const addComment = async (req,res,next) =>{
+    const newComment = new Comment({...req.body, userId: req.user.id});
+    try {
+        const savedComment = await newComment.save();
+        res.status(200).send(savedComment);
+    } catch (error) {
+        next(error);
+    }
+}
+
+// ADD COMMENT
+export const deleteComment = async (req,res,next) =>{
+    try {
+        const comment = await Comment.findById(req.params.id);
+        const video = await Video.findById(req.params.id);
+
+        if(req.user.id === comment.userId || req.user.id === video.userId){
+            await Comment.findByIdAndDelete(req.params.id);
+            res.status(200).send('The comment has been deleted.');
+        }else{
+             next(createError(403,'You can only delete your comment!'));
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+// GET COMMENT
+export const getComment = async (req,res,next) =>{
+    try {
+        const comments = await Comment.find({videoId:req.params.videoId});
+        res.status(200).send(comments);
+        
+    } catch (error) {
+        next(error);
+    }
+}
