d855ee19dd0fa4547407284838d2a0230c2a0a1a
reynaldos <reyjr98@gmail.com>
2022-12-06 12:46:33 -0500

    settings page began

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+
+// import { Link,useLocation } from "react-router-dom";
+
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+   <Container>
+      <Wrapper>
+    
+    </Wrapper>
+   </Container>
   )
 }
 
 export default Settings
+
+
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+
+`
+
+const Wrapper = styled.main`
+  width: calc(100% - 2rem);
+  max-width: 1000px;
+  height: calc(100% - 9rem);
+
+  background: ${({theme})=>theme.elementBG};
+	border: ${({theme})=>`solid ${theme.icon} ${theme.borderThickness}`};
+  border-radius: ${({theme})=>theme.borderRadius};
+
+  @media screen and (min-width: ${({theme}) => theme.breakpoint.sm}){
+    height: calc(100% - 10rem);
+  } 
+`
